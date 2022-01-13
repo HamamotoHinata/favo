5652f09879b42d477aa5a1b38f9d5108b3d7970b
2190003@ecccomp.com <hamamoto1999>
2022-01-14 00:46:11 +0900

    追加

---
+++ lib/photo_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PhotoListPage extends StatefulWidget {
+  @override
+  _PhotoListPageState createState() => _PhotoListPageState();
+}
+
+class _PhotoListPageState extends State<PhotoListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
